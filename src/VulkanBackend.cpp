5918edd022ce2c5a03147a3010947764f041a17e
Chris Libert <cbert745@gmail.com>
2026-09-06 00:40:33 -0700

    feat: Update graphics pipeline to disable backface culling

--- src/VulkanBackend.cpp
+++ src/VulkanBackend.cpp
@@ -944,7 +944,7 @@ void VulkanBackend::createGraphicsPipeline() {
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.cullMode = VK_CULL_MODE_NONE;
     rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
 
     VkPipelineMultisampleStateCreateInfo multisampling{};
